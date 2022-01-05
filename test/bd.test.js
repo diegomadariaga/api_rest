8f6e8af14dbac1a25f02bcc126e2182118d36699
diego madariaga <diegonicolasmadariaga@gmail.com>
2022-01-04 20:14:22 -0800

    agrupacion de test

--- test/bd.test.js
+++ test/bd.test.js
@@ -2,19 +2,22 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const {getUserById} = require('../build/database/db.js');
 
-test('getUserById', async () => {
-    const user = await getUserById(1);
-    expect(user[0].id).toBe(1);
-    expect(user[0].username).toBe('diego');
-    expect(user[0].password).toBe('string');
+describe('getUserById', () => {
+    test('debe retornar un usuario', async () => {
+        const user = await getUserById(1);
+        expect(user.id).toBe(1);
+        expect(user.username).toBe('diego');
+        expect(user.password).toBe('string');
+    });
+    
+    test('para un id que no existe, debe retornar undefined', async () => {
+        const user = await getUserById(85282);
+        expect(user).toBeUndefined();
+    });
+    
+    test('si no se pasa un id debe retornar undefined', async () => {
+        const user = await getUserById();
+        expect(user).toBeUndefined();
+    });
 });
 
-test('getUserById', async () => {
-    const user = await getUserById(85282);
-    expect(user[0]).toBeUndefined();
-});
-
-test('getUserById', async () => {
-    const user = await getUserById();
-    expect(user).toBeUndefined();
-});
